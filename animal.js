a3f463230213089ae225b0c6f400838c6cf0fce8
Mohammadalm <energykingdom@outlook.com>
2022-06-14 21:41:14 +0300

    adding files

---
+++ animal.js
@@ -0,0 +1,20 @@
+class Animal {
+    constructor(name, type) {
+        this.name = name;
+        this.type = type;
+
+
+    }
+    getName() {
+        console.log(this.name)
+    }
+
+
+    getType() {
+        console.log(this.type)
+    }
+
+
+}
+
+export default myAnimal()
---
+++ app.js
@@ -0,0 +1,45 @@
+// console.log("hello world");
+// var num = 10;
+
+// ++num;
+// for (let i = 0; i <= 20; i++) {
+//     if (i % 2 == 0) {
+//         console.log(i)
+//     }
+// }
+// console.log(num)
+//array
+//
+
+// let arr = ["blue", "yellow", "red"]
+
+// arr.forEach(printArr)
+
+// function printArr(item) {
+//     console.log(item)c;
+// class Animal {
+//     constructor(name, type) {
+//         this.name = name;
+//         this.type = type;
+
+
+//     }
+//     getName() {
+//         console.log(this.name)
+//     }
+
+
+//     getType() {
+//         console.log(this.type)
+//     }
+
+
+// }
+
+// let myAnimal = new Animal("Dog", "pitbull");
+// myAnimal.getName()
+// myAnimal.getType()
+import myAnimal from "/animal.js";
+var ann = new myAnimal("dog", "pitbull")
+
+console.log(this.name)
